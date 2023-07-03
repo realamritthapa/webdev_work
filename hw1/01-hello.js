d152958ea8d96df408a71fac77a8e1d1a16e3f07
Amrit Thapa <realamritthapa@gmail.com>
2023-07-02 22:06:50 -0700

    made image circle and logged about me
    need to use pseudo-element in first line

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,8 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+  console.log(
+    "My name is Amrit Thapa. I am a post-bacc student at Portland State University studying computer science. I graduated from Portland state in 2021 with a degree in biology with the goal to go into physical therapy. But after I graduated I changed my mind and enrolled in the cs program in 2022."
+  );
 }
