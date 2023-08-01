95e42135c5b4fe1e59a6d1e95062cf01a935c00a
Amrit Thapa <realamritthapa@gmail.com>
2023-08-01 16:34:37 -0700

    exercise 1 complete for now

--- hw3/01-hello.js
+++ hw3/01-hello.js
@@ -1,1 +1,12 @@
 // Add your code here
+const img = document.createElement("img");
+img.src = "../images/amrit.png";
+document.body.append(img);
+img.setAttribute("class", "img");
+
+const paragraph = document.createElement("p");
+paragraph.innerText =
+  " My name is Amrit Thapa. I am a post-bacc student at Portland State University studying computer science. I graduated from Portland state in 2021 with a degree in biology with the goal to go into physical therapy. But after I graduated I changed my mind and enrolled in the cs program in 2022.";
+
+document.body.append(paragraph);
+paragraph.setAttribute("class", "bio");
