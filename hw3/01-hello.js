4eae50d0a389266f570813c54e8e2b6000371244
Amrit Thapa <realamritthapa@gmail.com>
2023-08-04 15:28:34 -0700

    added multiple things

--- hw3/01-hello.js
+++ hw3/01-hello.js
@@ -1,12 +1,14 @@
 // Add your code here
+let main = document.querySelector("main");
 const img = document.createElement("img");
 img.src = "../images/amrit.png";
-document.body.append(img);
+main.appendChild(img);
 img.setAttribute("class", "img");
+img.setAttribute("alt", "Headshot of Amrit with black and white filter");
 
 const paragraph = document.createElement("p");
 paragraph.innerText =
   " My name is Amrit Thapa. I am a post-bacc student at Portland State University studying computer science. I graduated from Portland state in 2021 with a degree in biology with the goal to go into physical therapy. But after I graduated I changed my mind and enrolled in the cs program in 2022.";
 
-document.body.append(paragraph);
+main.appendChild(paragraph);
 paragraph.setAttribute("class", "bio");
--- hw3/03-starships.js
+++ hw3/03-starships.js
@@ -1,58 +1,115 @@
-const fetchData = (url) => {
+const fetchData = async (url) => {
   // REtrieve the data from the API
+  const response = await fetch(url);
+  const { results } = await response.json();
+
+  return results;
 };
+let starships;
+let result = fetchData("https://swapi.dev/api/starships/")
+  .then((data) => {
+    starships = data;
+    console.log(starships);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 const createSpaceshipComponent = (spaceship) => {
-  const container = document.createElement('section'); // do not modify this line
+  const container = document.createElement("section"); // do not modify this line
+  const name_credit = document.createElement("div");
+  name_credit.setAttribute("class", "name_credit");
+
+  const name_item = document.createElement("div");
+  name_item.setAttribute("class", "name");
+  name_item.innerText = spaceship.name;
+
+  const credit_item = document.createElement("div");
+  credit_item.setAttribute("class", "credit");
+  credit_item.innerText = spaceship.cost_in_credits;
+
+  const manufacture = document.createElement("div");
+  manufacture.setAttribute("class", "manufacture");
+  manufacture.innerText = `Manufactured by ${spaceship.manufacturer}`;
+
+  const speed_label = document.createElement("div");
+  speed_label.setAttribute("class", "speed_label");
+
+  const speed = document.createElement("div");
+  speed.setAttribute("class", "speed");
+  speed.innerText = spaceship.max_atmosphering_speed;
+
+  const speed_name = document.createElement("div");
+  speed_name.setAttribute("class", "speed_name");
+  speed_name.innerText = "Max atmosphering speed";
+  name_credit.appendChild(name_item);
+  name_credit.appendChild(credit_item);
+
+  container.appendChild(name_credit);
+  container.appendChild(manufacture);
 
   return container; // do not modify this line
 };
 
-const main = document.getElementsByTagName('main')[0];
+const main = document.getElementsByTagName("main")[0];
 
 const filterStarships = (input) => {
   // Return an array with all ships that have less than 10 passengers with more than one crew member
+  const filterd = input.filter((ships) => {
+    return (
+      (Number(ships.passengers) < 10 && Number(ships.crew) > 1) ||
+      ships.passengers == "n/a"
+    );
+    console.log(Number(ships.passengers));
+  });
+  console.log(filterd);
+  return filterd;
 };
 
 const reduceStarships = (input) => {
   // Return a String of the cost to purchase all ships in the input array
-  const totalCost = 0;
-
+  const totalCost = input.reduce((accumulator, currentValue) => {
+    const numCredit = Number(currentValue.cost_in_credits);
+    if (numCredit) {
+      return accumulator + numCredit;
+    }
+    return accumulator;
+  }, 0);
   return `The cost of all starships is ${totalCost.toLocaleString()} credits`;
 };
 
 // do not modify the code below
-let displayAllButton = document.getElementById('all');
-displayAllButton.addEventListener('click', () => {
+let displayAllButton = document.getElementById("all");
+displayAllButton.addEventListener("click", () => {
   displayShipComponents(starships);
 });
 
-let filterButton = document.getElementById('filter');
-filterButton.addEventListener('click', () => {
+let filterButton = document.getElementById("filter");
+filterButton.addEventListener("click", () => {
   const filteredShips = filterStarships(starships);
   displayShipComponents(filteredShips);
 });
 
-let reduceButton = document.getElementById('reduce');
-reduceButton.addEventListener('click', () => {
+let reduceButton = document.getElementById("reduce");
+reduceButton.addEventListener("click", () => {
   const totalCost = reduceStarships(starships);
   displayText(totalCost);
 });
 
 const clearAndReset = () => {
-  let app = document.getElementById('results');
+  let app = document.getElementById("results");
   app.remove();
-  app = document.createElement('div');
-  app.id = 'results';
-  app.style.display = 'flex';
-  app.style.flexWrap = 'wrap';
-  app.style.justifyContent = 'center';
+  app = document.createElement("div");
+  app.id = "results";
+  app.style.display = "flex";
+  app.style.flexWrap = "wrap";
+  app.style.justifyContent = "center";
   main.append(app);
 };
 
 const displayShipComponents = (starships) => {
   clearAndReset();
-  let app = document.getElementById('results');
+  let app = document.getElementById("results");
   for (const ship in starships) {
     const shipComponent = createSpaceshipComponent(starships[ship]);
     app.appendChild(shipComponent);
@@ -61,11 +118,11 @@ const displayShipComponents = (starships) => {
 
 const displayText = (text) => {
   clearAndReset();
-  let app = document.getElementById('results');
-  let paragraph = document.createElement('p');
+  let app = document.getElementById("results");
+  let paragraph = document.createElement("p");
   paragraph.textContent = text;
-  paragraph.style.backgroundColor = 'white';
-  paragraph.style.borderRadius = '10px';
-  paragraph.style.padding = '30px';
+  paragraph.style.backgroundColor = "white";
+  paragraph.style.borderRadius = "10px";
+  paragraph.style.padding = "30px";
   app.appendChild(paragraph);
 };
